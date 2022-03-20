1bd274e617102b0b72b3aa87e9377d2e2a45436a
AHaizaki <andoitz.haizaki@gmail.com>
2022-03-20 16:35:53 +0100

    QFile

---
+++ QFile/main.cpp
@@ -0,0 +1,35 @@
+#include <QCoreApplication>
+#include <QFile>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    QFile file;
+    file.setFileName("../QFile/Baldomero.txt");
+    if (!file.exists()) {
+        qDebug() << "Baldomero son los padres";
+    } else {
+        file.open(QIODevice::ReadOnly | QIODevice::Text);
+        if (!file.isOpen()) {
+            qDebug() << "No se pudo abrir el archivo";
+        } else {
+            QByteArray content;
+            content = file.readAll();
+            qDebug() << content;
+            file.close();
+        }
+        file.open(QIODevice::WriteOnly | QIODevice::Text);
+        if (!file.isOpen()) {
+            qDebug() << "No se pudo abrir el archivo";
+        } else {
+            QByteArray content;
+            content = "Jeje";
+            file.write(":)");
+            file.close();
+        }
+    }
+
+
+    return a.exec();
+}
