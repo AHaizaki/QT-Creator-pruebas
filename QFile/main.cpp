3ed0fd64309083200868b0cbdee6326071cdaeb3
AHaizaki <andoitz.haizaki@gmail.com>
2022-03-20 16:45:32 +0100

    QFile

--- QFile/main.cpp
+++ QFile/main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
         } else {
             QByteArray content;
             content = "Jeje";
-            file.write(":)");
+            file.write(content);
             file.flush();
             file.close();
         }
